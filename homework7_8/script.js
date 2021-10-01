097da966cc0902220897abcda0cfb8ad121d90ed
mikdikiy <mikdikiy11@gmail.com>
2021-10-01 12:03:38 +0300

    Add About content

--- homework7_8/script.js
+++ homework7_8/script.js
@@ -8,3 +8,23 @@ function checkType(){
   if (checkRadio3.checked){document.getElementsByClassName('carousel')[0].style.right = '200%';}
   if (checkRadio4.checked){document.getElementsByClassName('carousel')[0].style.right = '300%';}
 }
+function rightMove(){
+  var checkRadio1 = document.getElementById('radio-1');
+  var checkRadio2 = document.getElementById('radio-2');
+  var checkRadio3 = document.getElementById('radio-3');
+  var checkRadio4 = document.getElementById('radio-4');
+  if (checkRadio1.checked){checkRadio2.checked=true;} else
+  if (checkRadio2.checked){checkRadio3.checked=true;} else
+  if (checkRadio3.checked){checkRadio4.checked=true;}
+  checkType();
+}
+function leftMove(){
+  var checkRadio1 = document.getElementById('radio-1');
+  var checkRadio2 = document.getElementById('radio-2');
+  var checkRadio3 = document.getElementById('radio-3');
+  var checkRadio4 = document.getElementById('radio-4');
+  if (checkRadio4.checked){checkRadio3.checked=true;} else
+  if (checkRadio3.checked){checkRadio2.checked=true;} else
+  if (checkRadio2.checked){checkRadio1.checked=true;}
+  checkType();
+}
