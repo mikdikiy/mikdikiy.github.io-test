eb9c1429b4d8e5c89dd44f72699fe439b75c9375
mikdikiy <mikdikiy11@gmail.com>
2021-10-04 22:20:20 +0300

    Rework second slider to SCC only

--- homework7_8/script.js
+++ homework7_8/script.js
@@ -28,15 +28,3 @@ function leftMove(){
   if (checkRadio2.checked){checkRadio1.checked=true;}
   checkType();
 }
-function checkTestimonial(){
-  let checkRadio1 = document.getElementById('testimonial-btn-1');
-  let checkRadio2 = document.getElementById('testimonial-btn-2');
-  let checkRadio3 = document.getElementById('testimonial-btn-3');
-  let checkRadio4 = document.getElementById('testimonial-btn-4');
-  if (checkRadio1.checked)
-  {document.getElementsByClassName('testimonials-carousel')[0].style.right = '0';}
-  else if (checkRadio2.checked)
-  {document.getElementsByClassName('testimonials-carousel')[0].style.right = '120%';}
-  else if (checkRadio3.checked){document.getElementsByClassName('testimonials-carousel')[0].style.right = '240%';} else if (checkRadio4.checked){document.getElementsByClassName('testimonials-carousel')[0].style.right = '360%';}
-  else{document.getElementsByClassName('testimonial-4')[0].style.transform = 'scale(0,0)';}
-}
