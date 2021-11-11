b5a9647426511439cce60e7504339cecb2c68da4
mikdikiy <mikdikiy11@gmail.com>
2021-11-11 19:31:00 +0200

    Fix comments without npm

--- calculator/script.js
+++ calculator/script.js
@@ -1,105 +1,116 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var strCalc = "";
+document.addEventListener('DOMContentLoaded', function () {
+  let buttonsArray = [
+    'C',
+    'c',
+    'CE',
+    'ce',
+    '%',
+    'percent',
+    '/',
+    'div',
+    '7',
+    '7',
+    '8',
+    '8',
+    '9',
+    '9',
+    '*',
+    'mul',
+    '4',
+    '4',
+    '5',
+    '5',
+    '6',
+    '6',
+    '-',
+    'minus',
+    '1',
+    '1',
+    '2',
+    '2',
+    '3',
+    '3',
+    '+',
+    'plus',
+    '0',
+    '0',
+    '(',
+    'open-br',
+    ')',
+    'close-br',
+    '=',
+    'result',
+  ];
+  // buttonsArray.forEach(element => insertButton('.buttons',element,'btn-'+element));
+  for (let i = 0; i < buttonsArray.length; i += 2) {
+    insertButton(
+      '.buttons',
+      buttonsArray[i],
+      'btn-' + buttonsArray[i + 1],
+      i / 2
+    );
+  }
+  let strCalc = '';
   clearCalc();
-  document.querySelector(".btn-c").onclick = function () {
-    clearCalc();
+
+  document.querySelector('.input-string').oninput = function () {
+    strCalc = this.value;
   };
-  document.querySelector(".btn-ce").onclick = function () {
-    if (strCalc.length > 1) {
-      strCalc = strCalc.slice(0, strCalc.length - 1);
-      updateCalc();
-    } else {
-      clearCalc();
-    };
-  };
-  document.querySelector(".btn-percent").onclick = function () {
-    strCalc += "%";
-    updateCalc();
-  };
-  document.querySelector(".btn-div").onclick = function () {
-    strCalc += "/";
-    updateCalc();
-  };
-  document.querySelector(".btn-percent").onclick = function () {
-    strCalc += "%";
-    updateCalc();
-  };
-  document.querySelector(".btn-minus").onclick = function () {
-    strCalc += "-";
-    updateCalc();
-  };
-  document.querySelector(".btn-plus").onclick = function () {
-    strCalc += "+";
-    updateCalc();
-  };
-  document.querySelector(".btn-mul").onclick = function () {
-    strCalc += "*";
-    updateCalc();
-  };
-  document.querySelector(".btn-1").onclick = function () {
-    strCalc += "1";
-    updateCalc();
-  };
-  document.querySelector(".btn-2").onclick = function () {
-    strCalc += "2";
-    updateCalc();
-  };
-  document.querySelector(".btn-3").onclick = function () {
-    strCalc += "3";
-    updateCalc();
-  };
-  document.querySelector(".btn-4").onclick = function () {
-    strCalc += "4";
-    updateCalc();
-  };
-  document.querySelector(".btn-5").onclick = function () {
-    strCalc += "5";
-    updateCalc();
-  };
-  document.querySelector(".btn-6").onclick = function () {
-    strCalc += "6";
-    updateCalc();
-  };
-  document.querySelector(".btn-7").onclick = function () {
-    strCalc += "7";
-    updateCalc();
-  };
-  document.querySelector(".btn-8").onclick = function () {
-    strCalc += "8";
-    updateCalc();
-  };
-  document.querySelector(".btn-9").onclick = function () {
-    strCalc += "9";
-    updateCalc();
-  };
-  document.querySelector(".btn-0").onclick = function () {
-    strCalc += "0";
-    updateCalc();
-  };
-  document.querySelector(".btn-open-br").onclick = function () {
-    strCalc += "(";
-    updateCalc();
-  };
-  document.querySelector(".btn-close-br").onclick = function () {
-    strCalc += ")";
-    updateCalc();
-  };
-  document.querySelector(".btn-result").onclick = function () {
-    resultCalc();
-  };
+  document.querySelector('.btn-c').addEventListener('click', clearCalc);
+  document.querySelector('.btn-ce').addEventListener('click', backSymbol);
+  document.querySelector('.btn-percent').addEventListener('click', buttonClick);
+  document.querySelector('.btn-div').addEventListener('click', buttonClick);
+  document.querySelector('.btn-minus').addEventListener('click', buttonClick);
+  document.querySelector('.btn-plus').addEventListener('click', buttonClick);
+  document.querySelector('.btn-mul').addEventListener('click', buttonClick);
+  document.querySelector('.btn-1').addEventListener('click', buttonClick);
+  document.querySelector('.btn-2').addEventListener('click', buttonClick);
+  document.querySelector('.btn-3').addEventListener('click', buttonClick);
+  document.querySelector('.btn-4').addEventListener('click', buttonClick);
+  document.querySelector('.btn-5').addEventListener('click', buttonClick);
+  document.querySelector('.btn-6').addEventListener('click', buttonClick);
+  document.querySelector('.btn-7').addEventListener('click', buttonClick);
+  document.querySelector('.btn-8').addEventListener('click', buttonClick);
+  document.querySelector('.btn-9').addEventListener('click', buttonClick);
+  document.querySelector('.btn-0').addEventListener('click', buttonClick);
+  document.querySelector('.btn-open-br').addEventListener('click', buttonClick);
+  document.querySelector('.btn-close-br').addEventListener('click', buttonClick);
+  document.querySelector('.btn-result').addEventListener('click', resultCalc);
 
   function updateCalc() {
-    let inputString = document.getElementsByClassName("input-string")[0];
+    let inputString = document.getElementsByClassName('input-string')[0];
     inputString.value = strCalc;
     inputString.focus();
     inputString.selectionStart = inputString.value.length;
   }
   function clearCalc() {
-    strCalc = "";
-    document.getElementsByClassName("input-string")[0].value = 0;
+    strCalc = '';
+    document.getElementsByClassName('input-string')[0].value = 0;
   }
   function resultCalc() {
     strCalc = math.evaluate(strCalc);
     updateCalc();
+  }
+  function buttonClick() {
+    strCalc += this.value;
+    updateCalc();
+  }
+  function backSymbol() {
+    if (strCalc.length > 1) {
+      strCalc = strCalc.slice(0, strCalc.length - 1);
+      updateCalc();
+    } else {
+      clearCalc();
+    }
+  }
+  function insertButton(classAfter, symb, classButton, countButton) {
+    let addDiv = document.createElement('div');
+    document.querySelector(classAfter).append(addDiv);
+    addDiv.className = 'button-wrapper';
+    addDiv.innerHTML = `<button class="calc-button ${classButton}" value="${symb}">${symb}</button>`;
+    addDiv.style.cssText =
+      'width: 35px; height: 35px; margin-right: 5px; display: inline-block;';
+    document.getElementsByClassName('calc-button')[countButton].style.cssText =
+      'width: 100%; padding:10%;';
   }
 });
